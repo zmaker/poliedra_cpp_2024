85b56368aad37664c49302df2196b85cb1a2e711
Paolo Aliverti <paolo.aliverti@gmail.com>
2024-10-30 19:33:05 +0100

    l01
    .

---
+++ L01/P01_hello/main.cpp
@@ -0,0 +1,21 @@
+/*
+  Hello World
+  30/10/2024 - A.Paolo
+*/
+#include <iostream>
+
+using namespace std;
+
+//funzione principale
+int main()
+{
+  //stampo un messaggio
+  cout << "Hello world!" << endl;
+  cout << "Il mio primo programma in C++\n";
+  cout << "temp: " << 18 << " C\n";
+  /*
+  simboli speciali: + - * / % > < . , ; ?
+  */
+  cout << "10 + 5 = " << (10 + 5) << endl;
+  return 0;
+}
